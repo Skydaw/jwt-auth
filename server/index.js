fdb1976e7b3f0f67c4d182cd7f76970e4e74cea8
Sébastien Avenet <77531611+Skydaw@users.noreply.github.com>
2021-04-07 11:15:23 +0200

    cookie
    ajout cookies

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser= require('cookie-parser')
+
 const userRoutes = require('./routes/user.routes')
 
 // ajoute la connection dans un fichier séparé qui servira de module de connexion
@@ -11,8 +14,17 @@ mongoose.connect('mongodb://localhost/jwt_auth',{
 
 //Créer un fichier .env qui contient les variables d'environnement comme  l'URL
 const app = express();
+app.use(cookieParser())
+
+app.use(cors({
+    credentials:true,
+    origin:['http://localhost:8000','http://localhost:3000','http://localhost:8080']
+}))
+
 app.use(express.json());
 // Route
 app.use('/api/user', userRoutes);
+
+
 
 app.listen(8000);
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
+// permet de crypter les mots de passe
 const bcrypt = require('bcryptjs');
+// permet de creer des token de connection
+const jwt = require('jsonwebtoken')
+// appel le model de User
 const User = require('../models/user.model');
 
-router.get('/',(req,res)=>{
-    res.send('hello from user')
-});
 
 router.post('/register', async (req,res)=>{
     const salt = await bcrypt.genSalt();
@@ -23,4 +24,69 @@ router.post('/register', async (req,res)=>{
     res.send(data);
 
 })
+
+
+// rajout route / login
+router.post('/login',async (req,res)=>{
+    const user = await User.findOne({email:req.body.email});
+    // verifié le mail est dans la base de donnée
+    if(!user){
+        return res.status(404).send({
+            message:'Use not found'
+        })
+    }
+    // verifier que le mot de passe est valide
+    if(!await bcrypt.compare(req.body.password, user.password)){
+        return res.status(404).send({
+            message:'Invalid credentials'
+        })
+    }
+    // creer token de session
+    const token = jwt.sign({_id:user._id},'secret')
+    res.cookie('jwt',token,{
+        // uniquement pour des requete http
+        httpOnly:true,
+        //1 day en ms 
+        maxAge: 24 * 60 *60 *1000 
+    })
+    res.send(
+        {
+            message:'Authentification success'
+        }
+    )
+
+})
+// la route /user va servir a recuperer les infos de l'utilulisatuer authentifiée
+
+router.get('/', async (req,res)=>{
+
+    try{
+
+        const cookie = req.cookies['jwt'];
+        
+        const claims= await jwt.verify(cookie,'secret');
+        if(!claims){
+            return res.status(401).send({
+                message:'Not authentified'
+            })
+        }
+        const user = await User.findOne({_id:claims._id})
+        const {password, ...data}= await user.toJSON();
+        
+        res.send(data)
+    }
+    catch(error){
+        return res.status(401).send({
+            message:'Not authentified'
+        })
+    }
+})
+
+router.post('/logout',(req,res)=>{
+    res.cookie('jwt','',{maxAge:0});
+    res.send({
+        message:'Successfully logged out'
+    })
+})
+
 module.exports = router;
